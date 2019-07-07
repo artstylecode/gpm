90effc1456ef8ff4180e53dca8b8479a61f1bdab
artcoding <artcoding@localhost.localdomain>
2019-07-07 15:38:38 +0800

    父级包信息获取添加

--- main.go
+++ main.go
@@ -2,26 +2,49 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"regexp"
 	"strings"
 )
 
 func main() {
 	//logic.Install("")
-	getJson()
+	url := getConfigUrl()
+	_, _ = getConfigContent(url)
+	// reader := strings.NewReader(string(str))
+	// var config config.GpmConfig
+	// json.NewDecoder(reader).Decode(&config)
+
+	// fmt.Println(config.String())
 }
 
 const (
-	PATH    = "/blob/"
-	GITPATH = "github.com/artstylecode/gpm/search/gpm.json"
+	PATH    = "blob/master"
+	GITPATH = "github.com/artstylecode/gpm/gpm.json"
 )
 
-func getJson() {
+func getConfigContent(url string) ([]byte, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, nil
+	}
+	buff, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	ioutil.WriteFile("test.txt", buff, 0666)
+	//fmt.Println(string(buff))
+	return nil, err
+
+}
+func getConfigUrl() string {
 	reg, _ := regexp.Compile("[a-zA-Z.]+")
 	matchstr := reg.FindAllString(GITPATH, 10)
-	otherstr := matchstr[3:]
+
+	otherstr := strings.Join(matchstr[3:], "/")
 	matchstr = matchstr[0:3]
 	matchstr = append(matchstr, PATH)
-	fmt.Printf("%v\n", otherstr)
-	fmt.Printf("%s", strings.Join(matchstr, "/"))
+	matchstr = append(matchstr, otherstr)
+	return fmt.Sprintf("%s/%s", "https:/", strings.Join(matchstr, "/"))
 }
